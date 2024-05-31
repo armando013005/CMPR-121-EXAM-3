8036a33931db56aa2005e5c425789ae9c44b33fa
Armando Orozco <ao92436@student.sac.edu>
2024-05-31 12:33:06 -0700

    change file format

--- Exam 3/Exam 3/Declarations.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Rational.h"
-#include "input.h" // Include the custom input functions header
-#include <iostream>
-#include <string>
-
-using namespace std;
---
+++ Exam 3/Rational.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+
+namespace std {
+    class Rational {
+    private:
+        int numerator;
+        int denominator;
+
+        void Normalize();
+
+    public:
+        Rational();
+        Rational(int num, int denom);
+
+        void setNumerator(int num);
+        void setDenominator(int denom);
+
+        int getNumerator() const;
+        int getDenominator() const;
+
+        bool operator==(const Rational& other) const;
+        bool operator<(const Rational& other) const;
+
+        friend ostream& operator<<(ostream& out, const Rational& r);
+    };
+}
