e6d8b83e848e5dc03480937ba46a87e30c40292f
zendoctrine <168051790+zendoctrine@users.noreply.github.com>
2024-06-04 00:28:37 -0700

    Update Main.cpp
    Updating formatting of main menu display object. Will need to finalize the remaining segments missing and also update coloring and mismatched characters

--- Exam 3/Main.cpp
+++ Exam 3/Main.cpp
@@ -1,4 +1,4 @@
-﻿// Armando, Christopher, Duc, Thi
+// Armando, Christopher, Duc, Thi
 // CMPR121 - Professor Quach
 // Exam 3
 #include "Declarations.h"
@@ -12,7 +12,7 @@ int main() {
 
     do {
         displayMenu();
-        option = inputChar("\n\t\tChoose an option: ");
+        option = inputChar("\n\tChoose an option: ");
         switch (toupper(option)) {
         case 'V': {
             vectorMenu(rationalsVector);
@@ -45,62 +45,35 @@ int main() {
 
 void displayMenu() {
     cls();
-
-    // Stack and Queue section
-    blue;
-    cout << setw(84) << " " << string(1, char(186)) << "  in/out  " << string(1, char(186)) << "            " << string(1, char(186)) << "   in     " << string(1, char(186)) << endl;
-    cout << setw(84) << " " << string(1, char(186)) << " " << string(1, char(218)) << string(6, char(196)) << string(1, char(191)) << " " << setw(12) << " " << string(1, char(186)) << " " << string(1, char(218)) << string(6, char(196)) << string(1, char(191)) << " " << string(1, char(186)) << endl;
-    cout << setw(66) << " " << "top»" << string(1, char(186)) << " " << string(1, char(179)) << setw(7) << "-10/21" << string(1, char(179)) << " " << setw(12) << " " << string(1, char(186)) << " " << string(1, char(179)) << setw(7) << "-57/61" << string(1, char(179)) << " " << string(1, char(186)) << endl;
-    cout << setw(70) << " " << string(1, char(186)) << " " << string(1, char(195)) << string(6, char(196)) << string(1, char(180)) << " " << setw(12) << " " << string(1, char(186)) << " " << string(1, char(195)) << string(6, char(196)) << string(1, char(180)) << " " << string(1, char(186)) << endl;
-
-    // Vector section
-    red;
-    cout << setw(8) << " " << string(1, char(218)) << string(6, char(196)) << string(1, char(194)) << string(6, char(196)) << string(1, char(194)) << string(6, char(196)) << string(1, char(191)) << "      ";
-    yellow;
-    cout << string(1, char(218)) << string(6, char(196)) << string(1, char(194)) << string(6, char(196)) << string(1, char(194)) << string(6, char(196)) << string(1, char(191)) << "        ";
-    blue;
-    cout << string(1, char(186)) << " " << string(1, char(179)) << setw(2) << " " << setw(6) << " " << " " << string(1, char(186)) << "            " << string(1, char(179)) << setw(2) << " " << setw(6) << " " << " " << string(1, char(186)) << endl;
-    cout << setw(8) << " " << string(1, char(179)) << " -13/24 " << string(1, char(179)) << " " << string(1, char(179)) << "  3/94 " << string(1, char(179)) << " " << string(1, char(179)) << " -25/36 " << string(1, char(179)) << "    ";
-    yellow;
-    cout << string(1, char(195)) << string(6, char(196)) << string(1, char(194)) << string(6, char(196)) << string(1, char(194)) << string(6, char(196)) << string(1, char(180)) << "        ";
-    blue;
-    cout << string(1, char(186)) << " " << string(1, char(179)) << setw(2) << " " << setw(6) << " " << " " << string(1, char(186)) << "            " << string(1, char(179)) << setw(2) << " " << setw(6) << " " << " " << string(1, char(186)) << endl;
-    cout << setw(8) << " " << string(1, char(192)) << string(6, char(196)) << string(1, char(193)) << string(6, char(196)) << string(1, char(193)) << string(6, char(196)) << string(1, char(217)) << "    ";
-    yellow;
-    cout << string(1, char(179)) << setw(2) << " " << " " << string(1, char(179)) << " " << setw(2) << " " << string(1, char(179)) << "        ";
-    blue;
-    cout << string(1, char(186)) << " " << string(1, char(179)) << setw(2) << " " << setw(6) << " " << " " << string(1, char(186)) << "            " << string(1, char(179)) << setw(2) << " " << setw(6) << " " << " " << string(1, char(186)) << endl;
-    cout << setw(8) << " " << " " << string(1, char(192)) << " " << setw(2) << " " << "0" << setw(2) << " " << "1" << setw(2) << " " << "2" << setw(6) << " " << "           ";
-    yellow;
-    cout << string(1, char(179)) << " " << setw(2) << " " << setw(6) << " " << " " << string(1, char(179)) << "                                ";
-    blue;
-    cout << string(1, char(186)) << " " << string(1, char(179)) << setw(2) << " " << setw(6) << " " << " " << string(1, char(186)) << "            " << string(1, char(179)) << setw(2) << " " << setw(6) << " " << " " << string(1, char(186)) << endl;
-    cout << string(70, ' ') << string(1, char(179)) << "            " << string(1, char(179)) << "   out    " << string(1, char(179)) << endl;
-    cout << string(70, ' ') << string(1, char(192)) << string(6, char(196)) << string(1, char(193)) << string(6, char(196)) << string(1, char(217)) << " " << setw(6) << " " << string(1, char(192)) << string(6, char(196)) << string(1, char(217)) << endl;
-
+    cout << setw(76) << string(1, char(186)) << "  in / out    " << string(1, char(186)) << setw(12)  << string(1, char(186)) << "       in      " << string(1, char(186)) << endl;
+    cout << setw(76) << string(1, char(186)) << " " << string(1, char(218)) << string(10, char(196)) << string(1, char(191)) << " " << string(1, char(186)) << setw(12) << string(1, char(186)) << " " << string(1, char(218)) << string(11, char(196)) << string(1, char(191)) << " " << string(1, char(186)) << endl;
+    cout << setw(71) << " " << "top" << "»" << string(1, char(186)) << " " << string(1, char(179)) << setw(8) << "- 10 / 21 " << string(1, char(179)) << " " << string(1, char(186)) << setw(6) << " " << "rear" << "\u00BB" << string(1, char(186)) << " " << string(1, char(179)) << setw(8) << "- 57 / 61 " << " " << string(1, char(179)) << " " << string(1, char(186)) << endl;
+    cout << setw(75) << " " << string(1, char(186)) << " " << string(1, char(195)) << string(5, char(196)) << string(1, char(196)) << string(4, char(196)) << string(1, char(180)) << " " << string(1, char(186)) << setw(11) << " " << string(1, char(186)) << " " << string(1, char(195)) << string(6, char(196)) << string(1, char(196)) << string(4, char(196)) << string(1, char(180)) << " " << string(1, char(186)) << endl;
+    cout << setw(3) << string(1, char(218)) << string(8, char(196)) << string(1, char(194)) << string(6, char(196)) << string(1, char(194)) << string(8, char(196)) << string(1, char(191)) << setw(6) << string(1, char(218)) << string(9, char(196)) << string(1, char(179)) << " " << string(1, char(218)) << string(7, char(196)) << string(3, char(196)) << string(1, char(179)) << " " << string(1, char(218)) << string(7, char(196)) << string(1, char(196)) << string(1, char(196)) << string(1, char(196)) << string(1, char(186)) << setw(6) << string(1, char(186)) << " " << string(1, char(179)) << "- 31 / 41 " << string(1, char(179)) << " " << string(1, char(186)) << setw(11) << " " << string(1, char(186)) << " " << string(1, char(179)) << "  39 / 49  " << string(1, char(179)) << " " << string(1, char(186)) << endl;
+    cout << setw(2) << " " << string(1, char(179)) << "- 13/24 " << string(1, char(179)) << " 3/94 " << string(1, char(179)) << "- 25/36 " << string(1, char(179)) << setw(5) << string(1, char(187)) << string(1, char(179)) << " 11 / 23 " << string(1, char(186)) << string(1, char(187)) << string(1, char(179)) << "- 23 / 41 " << string(1, char(186)) << string(1, char(187)) << string(1, char(179)) << "- 53 / 61 " << string(1, char(186)) << setw(6) << string(1, char(186)) << " " << string(1, char(195)) << string(10, char(196)) << string(1, char(180)) << " " << string(1, char(186)) << setw(12) << string(1, char(186)) << " " << string(1, char(195)) << string(11, char(196)) << string(1, char(180)) << " " << string(1, char(186)) << endl;
+    cout << setw(3) << string(1, char(218)) << string(8, char(196)) << string(1, char(194)) << string(6, char(196)) << string(1, char(194)) << string(8, char(196)) << string(1, char(191)) << setw(6) << string(1, char(218)) << string(9, char(196)) << string(1, char(179)) << " " << string(1, char(218)) << string(7, char(196)) << string(3, char(196)) << string(1, char(179)) << " " << string(1, char(218)) << string(7, char(196)) << string(1, char(196)) << string(1, char(196)) << string(1, char(196)) << string(1, char(186)) << setw(6) << string(1, char(186)) << " " << string(1, char(179)) << "  73 / 61 " << string(1, char(179)) << " " << string(1, char(186)) << setw(11) << " " << string(1, char(186)) << " " << string(1, char(179)) << "- 71 / 72  " << string(1, char(179)) << " " << string(1, char(186)) << endl;
+    cout << setw(3) << "0" << setw(9) << "1" << setw(7) << "2" << setw(18) << "head" << setw(39) << ;
     res;
-    cout << "\nVector" << "                    " << "Linked List" << "                           " << "Stack" << "                   " << "Queue";
-    cout << "\n\nA container is a holder object that stores a collection of other objects (its elements). They\n"
-        << "are implemented as class templates, which allows great flexibility in the types supported as\n"
-        << "elements. The container manages the storage space for its elements and provides member functions\n"
-        << "to access them, either directly or through iterators(reference objects with similar properties\n"
-        << "to pointers).\n"
-        << "\nCMPR121 Exam3: STL (Standard Template Library) Containers and Container Adaptors by Prof Q (5-13-2024)\n"
-        << string(90, char(205)) << "\n";
+    cout << "\n\tVector" << setw(30) << "Linked List" << setw(40) << "Stack" << setw(30) << "Queue";
+    cout << "\n\n\tA container is a holder object that stores a collection of other objects (its elements). They"
+        << "\n\tare implemented as class templates, which allows great flexibility in the types supported as"
+        << "\n\telements. The container manages the storage space for its elements and provides member functions"
+        << "\n\tto access them, either directly or through iterators(reference objects with similar properties"
+        << "\n\tto pointers)."
+        << "\n\tCMPR121 Exam3 : STL(Standard Template Library) Containers and Container Adaptors by Armando, Chris, Duc, Thi"
+        << "\n\t" + string(90, char(205)) << "\n";
 
     red;
-    cout << "V. Vector Container\n";
+    cout << "\n\tV. Vector Container\n";
     yellow;
-    cout << "L. List Container\n";
+    cout << "\n\tL. List Container\n";
     blue;
-    cout << "S. Stack (LIFO) Container Adaptor\n";
+    cout << "\n\tS. Stack (LIFO) Container Adaptor\n";
     green;
-    cout << "Q. Queue (FIFO) Container Adaptor\n";
+    cout << "\n\tQ. Queue (FIFO) Container Adaptor\n";
     res;
-    cout << string(90, char(196)) << "\n"
-        << "X. Exit\n"
-        << string(90, char(205)) << "\n"
-        << "Option: ";
+    cout << "\n\t" + string(84, char(196)) << "\n"
+        << "\n\tX. Exit\n"
+        << "\n\t" + string(90, char(205)) << "\n"
+        << "\n\tOption: ";
 }
-
-
